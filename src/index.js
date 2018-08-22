60cb44f026d1366ea306474cf344983c793d7cfe
VICTOR JASSIEL DIAZ VELASQUEZ <B187926@bancoazteca.com.mx>
2018-08-22 13:11:07 -0500

    Paged Table

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Table from "./components/table";
 import Column from "./components/column";
+import PagedTable from "./components/pagedtable";
 import RowStyler from "./components/rowstyler";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSort, faCheckCircle, faSortUp, faSortDown, faFilter, faAngleUp, faAngleDown, faTimes, faCog } from "@fortawesome/free-solid-svg-icons";
@@ -7,4 +8,4 @@ import { faCircle } from "@fortawesome/free-regular-svg-icons";
 
 library.add(faSort, faCircle, faCheckCircle, faSortUp, faSortDown, faFilter, faAngleUp, faAngleDown, faTimes, faCog);
 
-export { Table, Column, RowStyler };
+export { Table, Column, PagedTable, RowStyler };
