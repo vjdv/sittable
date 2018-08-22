9477162d9b36d29bac0cd7fbe77da7ea32ebb4a0
VICTOR JASSIEL DIAZ VELASQUEZ <B187926@bancoazteca.com.mx>
2018-08-22 13:33:12 -0500

    Changed name to Styler for convention

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import Table from "./components/table";
 import Column from "./components/column";
 import PagedTable from "./components/pagedtable";
-import RowStyler from "./components/rowstyler";
+import Styler from "./components/rowstyler";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSort, faCheckCircle, faSortUp, faSortDown, faFilter, faAngleUp, faAngleDown, faTimes, faCog } from "@fortawesome/free-solid-svg-icons";
 import { faCircle } from "@fortawesome/free-regular-svg-icons";
 
 library.add(faSort, faCircle, faCheckCircle, faSortUp, faSortDown, faFilter, faAngleUp, faAngleDown, faTimes, faCog);
 
-export { Table, Column, PagedTable, RowStyler };
+export { Table, Column, PagedTable, Styler };
